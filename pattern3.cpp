c7d0a983a4b615b14d7a33ed56d7c586c601e2fb
AbhiNav-3007 <1DT23CY002@dsatm.edu.in>
2025-04-25 20:09:43 +0530

    corrected pattern3

--- pattern3.cpp
+++ pattern3.cpp
@@ -9,8 +9,8 @@
 using namespace std;
 void print3(int n){
     for(int i=1;i<=n; i++){
-        for(int j=1; j<=n ;j++){
-            cout<<" * ";
+        for(int j=1; j<=i ;j++){
+            cout<< j ;
         }cout<<endl;
     }
 }
